91a69274377a44f6e11576a43e5ed95e319ccc23
Keith Grennan <keith@nearlyfree.org>
2020-09-02 00:16:09 -0700

    basic clause recursion

--- src/mbchallenge/core.clj
+++ src/mbchallenge/core.clj
@@ -26,7 +26,7 @@
 (extend SQLServerDialect
   Dialect
   (merge base-dialect
-         {:with-limit (fn [limit [select & xs]]
+         {:with-limit (fn [_ limit [select & xs]]
                         (concat [select "TOP" limit] xs))}))
 
 (defmulti get-dialect identity)
@@ -37,10 +37,71 @@
 
 (defmethod get-dialect :sqlserver [_] (SQLServerDialect.))
 
+(defprotocol AsSQL
+  (as-sql [this]))
+
+; Literals
+(defrecord SQLNull [ctx])
+(defrecord SQLNumber [ctx value])
+
+; Clauses
+(defrecord SQLEquals [ctx args])
+(defrecord Field [ctx args])
+
+(declare parse-clause)
+
+(extend-protocol AsSQL
+  SQLNull
+  (as-sql [this] ["IS NULL"])
+  SQLNumber
+  (as-sql [this] [(-> (.-value this) str)])
+  SQLEquals
+  (as-sql [this] (concat
+                   (-> (.-args this) (first) (as-sql))
+                   ["="]
+                   (-> (.-args this) (last) (as-sql))))
+  Field
+  (as-sql [this] ["foo"]))
+
+(defmulti get-literal (fn [node _] (class node)))
+(defmethod get-literal nil [_ ctx] (SQLNull. ctx))
+(defmethod get-literal Long [node ctx] (SQLNumber. ctx node))
+
+(defmulti get-clause (fn [clause-id _ _] clause-id))
+(defmethod get-clause := [_ args ctx] (SQLEquals. ctx args))
+(defmethod get-clause :field [_ args ctx] (Field. ctx args))
+
+(defn maybe-with-limit [{dialect :dialect} limit sql]
+  (if limit
+    (with-limit dialect limit sql)
+    sql))
+
+(def is-clause? sequential?)
+
+(defmulti parse-node (fn [node ctx] (is-clause? node)))
+(defmethod parse-node true [node ctx]
+  (let [clause-id (first node)
+        clause-args (map #(parse-node % ctx) (rest node))
+        clause (get-clause clause-id clause-args ctx)]
+    (when-not clause
+      (throw (IllegalArgumentException. (str "Unknown clause " node))))
+    clause))
+(defmethod parse-node false [node ctx]
+  (if-let [literal (get-literal node ctx)]
+    literal
+    (throw (IllegalArgumentException. (str "Unknown literal " node)))))
+
+(defn maybe-with-where [ctx where sql]
+  (if where
+    (concat sql ["WHERE"] (as-sql (parse-node where ctx)))
+    sql))
+
 (defn generate-sql [dialect fields {where :where limit :limit}]
-  (let [sql-dialect (get-dialect dialect)]
+  (let [sql-dialect (get-dialect dialect)
+        ctx {:dialect sql-dialect :fields fields}]
     (->>
       select
-      (with-limit sql-dialect limit)
+      (maybe-with-where ctx where)
+      (maybe-with-limit ctx limit)
       (string/join " "))))
 
--- test/mbchallenge/core_test.clj
+++ test/mbchallenge/core_test.clj
@@ -12,6 +12,17 @@
     (is (=
           "SELECT * FROM data LIMIT 5"
           (generate-sql :postgres fields {:limit 5})))
-    #_(is (=
+    (is (=
+          "SELECT * FROM data LIMIT 5"
+          (generate-sql :mysql fields {:limit 5})))
+    (is (=
+          "SELECT TOP 5 * FROM data"
+          (generate-sql :sqlserver fields {:limit 5}))))
+  (testing "no limit"
+    (is (=
+          "SELECT * FROM data"
+          (generate-sql :postgres fields {}))))
+  (testing "basics"
+    (is (=
           "SELECT * FROM data WHERE date_joined IS NULL"
           (generate-sql :postgres fields {:where [:= [:field 3] nil]})))))
